6a330b850df6bd5c3f8a1c2106f6ae646edceae2
Manu <manuelmoualek@gmail.com>
2022-08-09 18:25:44 +0100

    fizzbuzz solution

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,2 +1,19 @@
 public class FizzBuzz {
+    public static void main(String[] args) {
+        fizzBuzz(100);
+    }
+
+    public static void fizzBuzz(int number){
+        for (int i = 0; i <= number; i++) {
+            if (i % 15 == 0){
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0){
+                System.out.println("Fizz");
+            } else if (i % 5 == 0){
+                System.out.println("Buzz");
+            } else{
+                System.out.println(i);
+            }
+        }
+    }
 }
